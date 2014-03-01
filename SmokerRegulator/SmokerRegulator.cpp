3fdf1f8cc0c763ac976a36f0bf182ab51bb99885
Sven Koster <svenmail@gmail.com>
2014-03-01 21:32:33 +0100

    SmokerRegulator class added. It uses a PID routine to determine how to react to temperature differences

---
+++ SmokerRegulator/SmokerRegulator.cpp
@@ -0,0 +1,51 @@
+/*
+ * SmokerRegulator.cpp
+ *
+ *  Created on: 1 mrt. 2014
+ *      Author: svenvandermeij
+ */
+#include <SmokerRegulator.h>
+
+//Define the aggressive and conservative Tuning Parameters
+double aggKp=4, aggKi=0.2, aggKd=1;
+double consKp=1, consKi=0.05, consKd=0.25;
+
+SmokerRegulator::SmokerRegulator(double *curTemp, double *targTemp, bool measure_in_meat, GasValve *gasvalve, SmokeValve *smokevalve) {
+	measured_in_meat = measure_in_meat;
+	currentTemp = curTemp;
+	targetTemp = targTemp;
+	smokeValve = smokevalve;
+	gasValve = gasvalve;
+	if (measured_in_meat) {
+		myPID = new PID(currentTemp, &output, targetTemp, consKp, consKi, consKd, DIRECT);
+	} else {
+		myPID = new PID(currentTemp, &output, targetTemp, aggKp, aggKi, aggKd, DIRECT);
+	}
+}
+
+void SmokerRegulator::start() {
+	myPID->SetMode(AUTOMATIC);
+}
+
+void SmokerRegulator::stop() {
+	myPID->SetMode(MANUAL);
+}
+
+void SmokerRegulator::calculate() {
+	double diff = *targetTemp - *currentTemp;
+
+	if (!measured_in_meat) {
+		if (diff < 10) { // now use conservative settings
+			myPID->SetTunings(consKp, consKi, consKd);
+		} else { // use aggressive settings
+			myPID->SetTunings(aggKp, aggKi, aggKd);
+		}
+	}
+	myPID->Compute();
+
+	// Now determine the gas/smoke valve settings and change them with 'output' from the PID routine as
+	// main parameter.
+
+}
+
+
---
+++ SmokerRegulator/SmokerRegulator.h
@@ -0,0 +1,29 @@
+/*
+ * SmokerRegulator.h
+ *
+ *  Created on: 1 mrt. 2014
+ *      Author: svenvandermeij
+ */
+
+#ifndef SMOKERREGULATOR_H_
+#define SMOKERREGULATOR_H_
+
+#include <PID_v1.h>
+#include <GasValve.h>
+
+class SmokerRegulator {
+protected:
+	PID *myPID;
+	bool measured_in_meat;
+	double *currentTemp, *targetTemp, output;
+	GasValve *gasValve;
+	SmokeValve *smokeValve;
+public:
+	SmokerRegulator(double *curTemp, double *targTemp, bool measure_in_meat, GasValve *gasvalve, SmokeValve *smokevalve);
+	void start(void);
+	void stop(void);
+	void calculate(void);
+
+};
+
+#endif /* SMOKERREGULATOR_H_ */
